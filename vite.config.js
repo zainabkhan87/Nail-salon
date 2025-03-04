977c9c208d8a6e566ea15b98f28ce35eed4844d9
zainabkhan87 <zainab.khann87@gmail.com>
2025-03-04 19:34:06 +0500

    task- done

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/your-repo-name/",
+  base: "beauty-salon",
   
 })
